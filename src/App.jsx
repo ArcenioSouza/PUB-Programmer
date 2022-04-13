2c6c751cd18d944865563669791bae1711739792
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-13 19:01:49 -0300

    feat: :sparkles:Configura app.jsx para receber as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-
+import { BrowserRouter } from "react-router-dom";
+import NavbarAdm from "./components/navbarAdm/NavbarAdm";
+import Footer from "./components/footer/FooterAdm";
+import RoutesApp from "./routes/Routes";
 
 function App() {
-  return (
-    <>
-    </>
-  );
+   return (
+      <BrowserRouter>
+         <NavbarAdm />
+         <RoutesApp />
+         <Footer />
+      </BrowserRouter>
+   );
 }
 
 export default App;
