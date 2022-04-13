adeb36fe8932687de71922432025a175853f6c8e
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-13 19:27:30 -0300

    feat: :sparkles:Adiciona page HomeAdm

---
+++ src/pages/homeAdm/HomeAdm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "./StyledHomeAdm";
+
+const HomeAdm = () => {
+   return (
+      <Container>
+         <h1>Selecione no menu acima a área que deseja acessar</h1>
+      </Container>
+   );
+};
+
+export default HomeAdm;
---
+++ src/pages/homeAdm/StyledHomeAdm.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import imgFundo from "../../assets/img/FundoApp.png"
+
+const Container = styled.main`
+   width: 100%;
+   height: 90%;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   font-family: var(--font-textos);
+   color: var(--branco);
+   background-image: url(${imgFundo}) ;
+
+   h1{
+      background-color: var(--pretobg);
+      width: 100%;
+      height: 200px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+   }
+`;
+export default Container;
