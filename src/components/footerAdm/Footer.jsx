6f0953f87521b619826936df38fb2583a59d1b58
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-13 19:17:39 -0300

    feat: :sparkles:Adiciona componente Footer

---
+++ src/components/footerAdm/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from './StyledFooter'
+
+const Footer = () => {
+  return (
+    <Container>
+       <p>Logado na sessão administrativa</p>
+    </Container>
+  )
+}
+
+export default Footer
