d7b4af50f089dfadaec2f2190501c691c7d37b52
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-13 19:13:10 -0300

    refactor: :recycle:Muda diretórios como novos nomes dos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import NavbarAdm from "./components/navbarAdm/NavbarAdm";
-import Footer from "./components/footer/FooterAdm";
+import Footer from "./components/footerAdm/Footer";
 import RoutesApp from "./routes/Routes";
 
 function App() {
