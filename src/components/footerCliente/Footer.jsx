c98f37f09df1ba76bebe0771cdaf4de5394d0126
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-18 20:16:57 -0300

    :sparkles: Adiciona componente Footer lado cliente

---
+++ src/components/footerCliente/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "./StyledFooter";
+
+const FooterCliente = () => {
+   return (
+      <Container>
+         <p>Logado na sessão administrativa</p>
+      </Container>
+   );
+};
+
+export default FooterCliente;
---
+++ src/components/footerCliente/StyledFooter.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.footer`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   width: 100%;
+   height: 40px;
+   font-family: var(--font-textos);
+   color: var(--branco);
+   background-color: var(--preto);
+   border-top: 1px solid var(--rosa);
+`;
+
+export default Container;
