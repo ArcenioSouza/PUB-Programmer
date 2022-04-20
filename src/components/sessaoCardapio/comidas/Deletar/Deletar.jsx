219bcb16d6a3446fb8c6968406f372f50a9aec8f
gicelle <gicelleds@gmail.com>
2022-04-19 22:21:35 -0300

    feat: :sparkles: Adiciona o componente deletar da sessão comidas

---
+++ src/components/sessaoCardapio/comidas/Deletar/Deletar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Button from "../../../button/Button";
+import Container from "./StyledDeletar.jsx";
+import { useForm } from "react-hook-form";
+import api from "../../../../services/api";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Deletar = () => {
+   const {register, handleSubmit} = useForm();
+
+   const onSubmit = (data) => {      
+      toast.promise(
+         api.delete(`/foods/${data.id}`),
+         {
+            pending: "Apagando informações da comida",
+            error: "Erro ao apagar os dados",
+            success: "Dados apagados com sucesso",
+         }
+        )    
+   }
+
+   return (
+      <Container>
+         <form>
+            <label htmlFor="id">Digite o id da comida </label>
+            <div>
+               <input
+                  type="text"
+                  {...register("id")}                
+               />
+               <Button text="Deletar" onClick={handleSubmit(onSubmit)}/>
+            </div>
+         </form>
+         <ToastContainer position="top-right" autoClose={3000}/>
+      </Container>
+   );
+};
+
+export default Deletar;
