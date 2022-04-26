10fc0e89f729ad461e341dd5dcb7c2c9c6f7c1d1
Matheus Camba <mhscamba@gmail.com>
2022-04-26 11:50:10 -0300

    feat: :sparkles:Add Input

---
+++ src/components/input/StyledInput.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const InputStyled = styled.input`
+  background: rgba(255, 255, 255, 0.15);
+  width: 60%;
+  height: 32px;
+  padding: 10px;
+  outline: none;
+  border: none;
+  border-radius: 10px;
+  font-size: 18px;
+  color: var(--preto);
+`;
