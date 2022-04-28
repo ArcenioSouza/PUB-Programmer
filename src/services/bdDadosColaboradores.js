7105274ceff0cfd62f3fd7687d8bda2bdb128206
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-28 12:05:27 -0300

    feat: :sparkles: Adiciona banco de dados com informações dos colaboradores

---
+++ src/services/bdDadosColaboradores.js
@@ -0,0 +1,95 @@
+const funcionarios = [
+   {
+      id: "1",
+      nome: "Arcenio Souza",
+      idade: "35 anos",
+      cargo: "Desenvolvedor FullStack e professor de Educação Física",
+      nascimento: "Nascido em São Paulo",
+      anoAdmissao: "Colaborador desde 2022",
+      hobbie: "Seu hobbie é viajar e praticar esportes, principalmente futebol"
+   },
+   {
+      id: "2",
+      nome: "Hugo Parada",
+      idade: "29 anos",
+      cargo: "Desenvolvedor FullStack",
+      nascimento: "Nascido no Rio de Janeiro",
+      anoAdmissao: "Colaborador desde 2022",
+      hobbie: "Seu hobbie é comer churrasco, beber cerveja e jogar bola"
+   },
+   {
+      id: "3",
+      nome: "Gicelle Sena",
+      idade: "",
+      cargo: "",
+      nascimento: "",
+      anoAdmissao: "",
+      hobbie: ""
+   },
+   {
+      id: "4",
+      nome: "Matheus Camba",
+      idade: "25",
+      cargo: "Desenvolvedor FullStack",
+      nascimento: "Nascido no Rio de Janeiro",
+      anoAdmissao: "Colaborador desde 2022",
+      hobbie: "Sair com os amigos, tocar violão e curtir uma praia."
+   },
+   {
+      id: "5",
+      nome: "Milena Souza",
+      idade: "",
+      cargo: "",
+      nascimento: "",
+      anoAdmissao: "",
+      hobbie: ""
+   },
+   {
+      id: "6",
+      nome: "Emmie Petit",
+      idade: "31",
+      cargo: "Gerente",
+      nascimento: "Nascida em São Paulo",
+      anoAdmissao: "Colaboradora desde 2019",
+      hobbie: "Seu hobbie sempre foi cozinhar. Adora reunir a família e cozinhar para eles."
+   },
+   {
+      id: "7",
+      nome: "Katie Bailey",
+      idade: "57",
+      cargo: "Cozinheira",
+      nascimento: "Nascida em São Paulo",
+      anoAdmissao: "Colaboradora desde 2015",
+      hobbie: "Seu hobbie é praticar exercícios ao ar livre, adora a sensação de estar em meio a natureza se exercitando"
+   },
+   {
+      id: "8",
+      nome: "Elliot Jones",
+      idade: "42",
+      cargo: "Barman",
+      nascimento: "Nascido em Salvador, Bahia",
+      anoAdmissao: "Colaborador desde 2018",
+      hobbie: "Seu hobbie é tocar violão, adora juntar os amigos para bater um papo, tomar uma cerveja e tocar para eles"
+   },
+   {
+      id: "9",
+      nome: "Howard Elliot",
+      idade: "37",
+      cargo: "Copeiro",
+      nascimento: "Nascido em Curitiba, Paraná",
+      anoAdmissao: "Colaborador desde 2018",
+      hobbie: "Seu hobbie é fotografar, nos momentos livres procura visitar museus, exposições de arte e paisagens que ele possa registrar com suas lentes"
+   },
+   {
+      id: "10",
+      nome: "Magnus Larsen",
+      idade: "25",
+      cargo: "Auxiliar de Cozinha",
+      nascimento: "Nascido no Acre",
+      anoAdmissao: "Colaborador desde 2020",
+      hobbie: "Seu hobbie é pescar, para ele essa pratica é uma meditação"
+   },
+
+]
+
+export default funcionarios;
