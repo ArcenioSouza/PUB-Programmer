05cf4646ae7490db944e51bb5ab1b88903b0acad
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-29 17:34:57 -0300

    feat: :sparkles: Adiciona a página de cardápio lado cliente

--- src/pages/cardapioClientes/CardapioClientes.jsx
+++ src/pages/cardapioClientes/CardapioClientes.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Container } from "./StyledCardapio";
+import Button from "../../components/button/Button";
+import Porcoes from "../../assets/img/Porcoes.png";
+import { useParams } from "react-router";
+import ListaComidas from "../../components/listaDeComidas/ListaComidas";
+import ListaBebidas from "../../components/listaDeBebidas/ListaBebidas";
 
 const CardapioClientes = () => {
-  return (
-    <div>Cardapio Clientes</div>
-  )
-}
+
+  const {opcao} = useParams()
+
+   return (
+      <Container>
+         <h1>Escolha um cardápio</h1>
+
+         <div className="containerEscolhaCardapio">
+            <div className="botoes">
+               <NavLink to={"/cardapio/comidas"}>
+                  <Button text="Comidas" id="btnComidas"/>
+               </NavLink>
+               <NavLink to={"/cardapio/bebidas"}>
+                  <Button text="Bebidas"/>
+               </NavLink>
+            </div>
+            <div className="image">
+              { opcao === undefined ?
+                <img id="imageInicio" src={Porcoes} alt="" />
+                :
+                opcao === "comidas" ?
+                <ListaComidas/>
+                :
+               <ListaBebidas/>
+              }               
+            </div>
+         </div>
+      </Container>
+   );
+};
 
 export default CardapioClientes;
